a78bec2d8ca1f410e7287e312081b9e242dee3dd
Jessen <jessen123ptk@gmail.com>
2024-11-20 21:04:54 +0700

    Fix Footer UI

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,76 +1,60 @@
-<footer class="container py-5">
-    <div class="row">
-        <div class="col-12 col-md">
-            <div class="overflow-hidden" style="width: 40px; height: 40px;">
-                <img src="{{ asset('img/PustakaBekas.png') }}" alt=""
-                    style="width: 100%; height: 100%; object-fit: cover;">
+<footer class="container-fluid py-5">
+    <div class="row justify-content-center">
+        <div class="col-11">
+            <div class="row d-flex flex-wrap">
+                <div class="col-12 col-lg-3 mb-3">
+                    <div class="overflow-hidden mb-2" style="width: 40px; height: 40px;">
+                        <img src="{{ asset('img/PustakaBekas.png') }}" alt="PustakaBekas Logo">
+                    </div>
+                    <small class="d-block mb-4 text-body-secondary">&copy; 2024 PustakaBekas</small>
+                </div>
+
+                <div class="col-6 col-lg-3 mb-3">
+                    <h5>Contact Us</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="link-secondary text-decoration-none" href="mailto:info@pustakabekas.com">Email:
+                                info@pustakabekas.com</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="tel:+62123456789">Phone: +62 812 4567
+                                7890</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Address: Jl. Example Street,
+                                Jakarta</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6 col-lg-2 mb-3">
+                    <h5>Help</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="link-secondary text-decoration-none" href="#">FAQ</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Customer Support</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Shipping & Returns</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6 col-lg-2 mb-3">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="link-secondary text-decoration-none" href="#">Our Team</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Privacy Policy</a></li>
+                        <li><a class="link-secondary text-decoration-none" href="#">Terms of Service</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6 col-lg-2 mb-3">
+                    <h5>Follow Us</h5>
+                    <ul class="d-flex flex-column list-unstyled text-small gap-1">
+                        <li class="d-inline-flex"><i class="bi bi-instagram me-2"></i><a
+                                class="link-secondary text-decoration-none"
+                                href="https://www.instagram.com/pustakabekas" target="_blank">Instagram</a></li>
+                        <li class="d-inline-flex"><i class="bi bi-tiktok me-2"></i><a
+                                class="link-secondary text-decoration-none" href="https://www.tiktok.com/@pustakabekas"
+                                target="_blank">TikTok</a></li>
+                        <li class="d-inline-flex"><i class="bi bi-linkedin me-2"></i> <a
+                                class="link-secondary text-decoration-none"
+                                href="https://www.linkedin.com/company/pustakabekas" target="_blank">LinkedIn</a></li>
+                    </ul>
+                </div>
             </div>
-            <small class="d-block mt-2 mb-3 text-body-secondary">&copy; 2024</small>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Contact Us</h5>
-            <ul class="list-unstyled text-small">
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Email: info@pustakabekas.com</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Phone: +62 123 456 789</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Address: Jl. Example Street,
-                        Jakarta</a>
-                </li>
-        </div>
-        {{-- <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Resource name</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Resource</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Another resource</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Final resource</a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Business</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Education</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Government</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Gaming</a>
-                </li>
-            </ul>
-        </div> --}}
-        <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Team</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Locations</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Privacy</a>
-                </li>
-                <li>
-                    <a class="link-secondary text-decoration-none" href="#">Terms</a>
-                </li>
-            </ul>
         </div>
     </div>
 </footer>
